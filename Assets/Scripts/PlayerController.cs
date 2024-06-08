8e21828f9e75d47260265e9b40a968c2d230fb3d
jon1-santos <gupasantos@gmail.com>
2024-06-08 20:38:58 -0300

    feat: player controller slider colliders implemented

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,72 +2,110 @@ using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-{
-    private Vector2 inputDirection; // Input de movimentação
-    private Vector2 currentVelocity = Vector2.zero; // Como velocidade se trata de uma grandeza vetorial a mesma deve conter módulo (magnitude, direção e sentido)
-    private Rigidbody2D rb; // Referência ao componente Rigidbody2D do jogador
+{   
+    private Rigidbody2D rb; // Componente RigidBody para fins de cálculos físicos
+    private float moveHorizontal; // Input de movimento horizontal
+    private float moveVertical; // Input de movimento vertical
+    private Vector2 inputDirection; // Vetor para armazenar a direção do objeto
+    private Vector2 currentVelocity = Vector2.zero; // Vetor para armazenar a velocidade do objeto
+    public MoveOptions moveOptions; // Instância para configurações de propriedades relacionadas a movimentação
+    private Vector2 newPosition;
 
-    private Animator animator;
+    private Animator animator; // Componente para aplicação de animação
+ 
+    private float lastHorizontalDirection; // Var para identificar a última posição após uma movimentação (para fins de animação)
+    private bool animationIsMoving; // Var para identificar se o objeto está parado ou em movimento (para fins de animação)
 
-    private float lastHorizontalDirection;
-    private bool animationIsMoving;
-    public ColliderOptions colliderOptions;
-    public InteractableOptions interactableOptions;
-
-    public MoveOptions moveOptions;
-
+    public ColliderOptions colliderOptions; // Componente para identificação objetos colidíveis 
+    private LayerMask solidObjectsLayer;
+    public InteractableOptions interactableOptions; // Componente para identificação de objetos interativos
+    private LayerMask interactableObjectsLayer;
+    private bool isWakablePos;
     void Awake(){
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
-        colliderOptions.layer = LayerMask.GetMask("SolidObjects");
-        interactableOptions.layer = LayerMask.GetMask("InteractableObjects");
+        solidObjectsLayer = LayerMask.GetMask("SolidObjects");
+        interactableObjectsLayer = LayerMask.GetMask("InteractableObjects");
     }
-
     void Update()
     {
-        
-        float moveHorizontal = Input.GetAxisRaw("Horizontal"); // Pega o eixo de movimentação horizontal (em qualquer dispositivo) sem filtro de suavização
-        float moveVertical = Input.GetAxisRaw("Vertical");
-        bool blink = Input.GetKeyDown(KeyCode.J);
-
+        SetInputs();
+        Skills();
+        UpdateAnimation();
+    }
+    private void SetInputs(){
+        moveHorizontal = Input.GetAxisRaw("Horizontal"); // Pega o eixo de movimentação horizontal (em qualquer dispositivo) sem filtro de suavização
+        moveVertical = Input.GetAxisRaw("Vertical");
         inputDirection = new Vector2(moveHorizontal, moveVertical).normalized;
-        lastHorizontalDirection = moveHorizontal != 0 ? moveHorizontal : lastHorizontalDirection;
+        SlideColliders();
+    }
+    private void SlideColliders(){
+         if(!isWakablePos){
+            if(newPosition.x != rb.position.x){
+                if(inputDirection.y > 0) inputDirection.Set(0, 1);
+                if(inputDirection.y < 0) inputDirection.Set(0, -1);    
+            }
+            if(newPosition.y != rb.position.y){
+                if(inputDirection.x < 0) inputDirection.Set(-1, 0);
+                if(inputDirection.x > 0) inputDirection.Set(1, 0);
+            }
+        }
+    }
+    private void Skills(){
+        Blink();
+    }
+    private void Blink(){
+        bool blink = Input.GetKeyDown(KeyCode.J);
+        if(blink){
+             Vector2 targetPos = rb.position + inputDirection * 2;
+            if(IsWalkable(targetPos)){
+                rb.position = newPosition;
+            }else{
+                return;
+            }
+        };
+    }
+    private void UpdateAnimation(){
+        if(moveHorizontal != 0){
+            lastHorizontalDirection = moveHorizontal;
+        }
         
-        if(blink) rb.position += inputDirection * 2;
-
         animator.SetFloat("moveX", lastHorizontalDirection);
         animator.SetBool("isMoving", animationIsMoving);
     }
-
     void FixedUpdate()
     {
-        // Calcula a nova velocidade
-        if (inputDirection.magnitude > 0)
+        Move();
+    }
+    private void Move(){
+        SetVelocity();
+        SetNewPosition();
+    }
+    private void SetVelocity()
+    {
+        if(inputDirection.magnitude > 0)
         {
             currentVelocity = Vector2.Lerp(currentVelocity, inputDirection * moveOptions.maxSpeed, Time.fixedDeltaTime * moveOptions.acceleration); 
+            animationIsMoving = true;
         }
         else
         {
             currentVelocity = Vector2.Lerp(currentVelocity, Vector2.zero, Time.fixedDeltaTime * moveOptions.deceleration);
         }
-
-        // Move o personagem com base na nova velocidade calculada
-        Move();
     }
-
-    void Move()
+    private void SetNewPosition()
     {
        if (currentVelocity.magnitude > 0.1f) // Um treshold para verificar se está realmente se movendo
         {
-            Vector2 newPosition = rb.position + currentVelocity * Time.fixedDeltaTime;
-            if (IsWalkable(newPosition))
-            {
+            animationIsMoving = true;
+            newPosition = rb.position + currentVelocity * Time.fixedDeltaTime;
+            if(IsWalkable(newPosition)){
                 rb.MovePosition(newPosition);
-                animationIsMoving = true;
-            }
-            else
+                isWakablePos = true;
+            }else
             {
-                currentVelocity = Vector2.zero; // Parar o movimento ao colidir
+                isWakablePos = false;
+                currentVelocity = Vector2.zero;
             }
         }
         else
@@ -75,14 +113,13 @@ public class PlayerController : MonoBehaviour
             animationIsMoving = false;
         }
     }
-
-    bool IsWalkable(Vector2 targetPos)
+    private bool IsWalkable(Vector2 targetPos)
     {
-        var collider = Physics2D.OverlapCircle(targetPos, colliderOptions.radius, colliderOptions.layer);
-        var interactable = Physics2D.OverlapCircle(targetPos, interactableOptions.radius, interactableOptions.layer);
-        return  collider == null && interactable == null;
+        var collider = Physics2D.OverlapCircle(targetPos, colliderOptions.radius, solidObjectsLayer);
+        var interactable = Physics2D.OverlapCircle(targetPos, interactableOptions.radius, interactableObjectsLayer);
+        return collider == null && interactable == null;
+        
     }
-
     
     [Serializable] // Faz com que instâncias públicas dessa classe apareçam no inspector
     public class MoveOptions
@@ -92,17 +129,15 @@ public class PlayerController : MonoBehaviour
         public float deceleration = 5.5f;  // Taxa de desaceleração
     }
 
-    [Serializable] // Faz com que instâncias públicas dessa classe apareçam no inspector
+    [Serializable]
     public class ColliderOptions
     {
-        public LayerMask layer;
         public float radius = 0.2f;
     }
 
-    [Serializable] // Faz com que instâncias públicas dessa classe apareçam no inspector
+    [Serializable]
     public class InteractableOptions
     {
-        public LayerMask layer;
         public float radius = 0.3f;
     }
 }
